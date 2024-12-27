dd787bf41572da6934f610f21f47f459a6f4a348
yeoyeong <yeoyyeong@gmail.com>
2024-12-27 12:08:59 +0900

    프로젝트 생성

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      screens: {
+        mobile: "500px",
+      },
+      colors: {
+        "brand-1": "#1C2134",
+        "brand-2": "#377DFF",
+      },
+      fontFamily: {
+        sans: ["Pretendard"],
+      },
+      fontSize: {
+        "heading/h1": ["40px", { lineHeight: "140%", fontWeight: "700" }],
+        "heading/h6": ["16px", { lineHeight: "140%", fontWeight: "700" }],
+      },
+    },
+  },
+  plugins: [],
+};
