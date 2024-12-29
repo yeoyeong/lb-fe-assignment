8654ac92ce631c8704e3b6333e6d68e3f52c3a98
yeoyeong <yeoyyeong@gmail.com>
2024-12-29 12:56:25 +0900

    feat : 리액트 쿼리 설정

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,10 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const queryClient = new QueryClient(); // 생성
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
